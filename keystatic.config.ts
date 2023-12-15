ea743a34a222bf5fea7662317f9257433b1f647a
Dishan Sachin <134765302+dishansa@users.noreply.github.com>
2023-12-15 18:24:37 +0000

    Refactor keystatic.config.ts to include additional fields to posts

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,25 +1,60 @@
-import { config, collection, fields } from '@keystatic/core';
+import { config, collection, fields } from "@keystatic/core";
 
 export default config({
-  storage: {
-    kind: 'local',
-  },
-  collections: {
-    posts: collection({
-      label: 'Posts',
-      slugField: 'title',
-      path: 'content/posts/*',
-      format: { contentField: 'content' },
-      schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
-        content: fields.document({
-          label: 'Content',
-          formatting: true,
-          dividers: true,
-          links: true,
-          images: true,
-        }),
-      },
-    }),
-  },
+	storage: {
+		kind: "local",
+	},
+	collections: {
+		posts: collection({
+			label: "Posts",
+			slugField: "title",
+			path: "content/posts/*",
+			format: { contentField: "content" },
+			schema: {
+				title: fields.slug({
+					name: {
+						label: "Title",
+						description: "The title of the post",
+					},
+				}),
+				coverImage: fields.image({
+					label: "Cover Image",
+					description: "The cover image of the post",
+					directory: "public/images/avatars",
+					publicPath: "/images/avatars/",
+					validation: {
+						isRequired: false,
+					},
+				}),
+				content: fields.document({
+					label: "Content",
+					description: "The content of the post",
+					formatting: true,
+					dividers: true,
+					links: true,
+					images: true,
+				}),
+				createdAt: fields.date({
+					label: "Created At",
+					description: "The date this post was created",
+					validation: {
+						isRequired: true,
+					},
+				}),
+				updatedAt: fields.date({
+					label: "Updated At",
+					description: "The date this post was last updated",
+					validation: {
+						isRequired: false,
+					},
+				}),
+				draft: fields.checkbox({
+					label: "Draft",
+					description:
+						"Set this post as draft to prevent it from being published",
+					defaultValue: true,
+				}),
+			},
+		}),
+	},
 });
